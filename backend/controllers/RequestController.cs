b4822c6f7ccf07c1ac8548c9ee688e4745df5d82
Alex Van Alstyne <amvanalstyne@crimson.ua.edu>
2025-03-29 13:40:24 -0500

    original commit

---
+++ backend/controllers/RequestController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hackathon.project.HackathonProject.backend.controllers
+{
+    [Route("[controller]")]
+    public class RequestController : Controller
+    {
+        private readonly ILogger<RequestController> _logger;
+
+        public RequestController(ILogger<RequestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ backend/database/Database.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hackathon.project.HackathonProject.backend.database
+{
+    public class Database
+    {
+        private string cs;
+
+
+        public Database()
+        {
+            cs = "server=tuy8t6uuvh43khkk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;database=wypwpiobd711vs7a;user=c6w21g0kuls6ov8a;password=u1cyr20tjjxa2zz9;port=3306;";
+        }
+    }
+}
